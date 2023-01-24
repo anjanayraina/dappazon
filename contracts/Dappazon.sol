32429ff5c22a226be54501d7e573342c8a72bc9e
anjanay <anjanayraina326@gmail.com>
2023-01-24 23:44:05 +0530

    Major Changes in Contract

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -6,17 +6,31 @@ contract Dappazon {
     address public immutable owner;
     using Counters for Counters.Counter;
     Counters.Counter itemIndex;
+    Counters.Counter orderIndex;
     constructor() payable {
        owner = msg.sender;
     }
     //stores all the items in the store 
     mapping(uint => Item) public items;
+    // stores all the sellers in the store
     mapping(address=>Seller) sellers;
+    // all the order history of the clients
+    mapping(uint => Order) orders;
     enum ProductStatus{outOfStock , inStock , takenDown}
-
+    enum DeliveryStatus{ordered , dispatched , delivered }
     modifier isSeller(){
          require(keccak256(abi.encodePacked(sellers[msg.sender].name))!= keccak256(abi.encodePacked("")));
          _;
+    }
+
+    modifier isItemOwner(uint index){
+        require(items[index].owner == msg.sender , "Only item owner can make changes");
+        _;
+    }
+
+    modifier canBeBought(uint index){
+        require(items[index].status == ProductStatus.inStock);
+        _;
     }
     struct Seller{
         string name;
@@ -34,6 +48,13 @@ contract Dappazon {
         ProductStatus status;
 
     }
+
+    struct Order{
+        address buyer;
+        address seller;
+        uint itemIndex;
+        DeliveryStatus status ;
+    }
     // adds Items in the list 
     function addItem (
      string memory _name , 
@@ -41,12 +62,33 @@ contract Dappazon {
      string memory _image, 
      uint _cost , 
      uint _rating 
-     ) public isSeller{
+     ) external isSeller{
     Item memory item= Item(msg.sender , itemIndex.current() , _name , _cateogry , _image , _cost , _rating  , ProductStatus.inStock);
     items[itemIndex.current()] = item;
     }
     // to add a seller to the marketplace
-    function addSeller(string memory _name ) public {
+    function addSeller(string calldata _name ) public {
         sellers[msg.sender] = Seller(_name , msg.sender);
+    }
+
+    function changeItemAvaliability(uint index, uint change ) external isItemOwner(index){
+        if(change == 0)items[index].status = ProductStatus.outOfStock ;
+
+        else if(change == 1)items[index].status = ProductStatus.inStock;
+
+        else{
+            items[index].status= ProductStatus.takenDown; 
+        }
+
+ 
+    }
+
+    function buyItem(uint index) external  canBeBought(index){
+        Item memory item  = items[index];
+
+    }
+
+    function changeDelieryStatus( uint index,uint change) external  isSeller isItemOwner(index){
+        Order memory order  =orders[index]; 
     }
 }
