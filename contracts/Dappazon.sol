7cf163f8abd8fc24e970186f2045b4a9dff4c552
anjanay <anjanayraina326@gmail.com>
2023-01-24 21:33:21 +0530

    Main Contract Created

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 import 'github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol';
 contract Dappazon {
@@ -14,6 +13,11 @@ contract Dappazon {
     mapping(uint => Item) public items;
     mapping(address=>Seller) sellers;
     enum ProductStatus{outOfStock , inStock , takenDown}
+
+    modifier isSeller(){
+         require(keccak256(abi.encodePacked(sellers[msg.sender].name))!= keccak256(abi.encodePacked("")));
+         _;
+    }
     struct Seller{
         string name;
         address wallet;
@@ -23,6 +27,7 @@ contract Dappazon {
         uint id;
         string name;
         string cateogry; 
+        // image will be a link to the ipfs file 
         string image;
         uint cost;
         uint rating ;
@@ -30,14 +35,18 @@ contract Dappazon {
 
     }
     // adds Items in the list 
-    function addItem(
+    function addItem (
      string memory _name , 
      string memory _cateogry , 
      string memory _image, 
      uint _cost , 
      uint _rating 
-     ) public {
+     ) public isSeller{
     Item memory item= Item(msg.sender , itemIndex.current() , _name , _cateogry , _image , _cost , _rating  , ProductStatus.inStock);
     items[itemIndex.current()] = item;
+    }
+    // to add a seller to the marketplace
+    function addSeller(string memory _name ) public {
+        sellers[msg.sender] = Seller(_name , msg.sender);
     }
 }
