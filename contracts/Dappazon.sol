108f8eaa0e7e4411ee79950b2c16dd91edad2625
anjanay <anjanayraina326@gmail.com>
2023-01-24 16:42:32 +0530

    Some more changes in the contract

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.9;
 
 contract Dappazon {
     string public constant name = "Dappazon";
+    address public immutable owner;
+    constructor() payable {
+       owner = msg.sender;
+    }
 }
