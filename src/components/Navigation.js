ddb259f93a263d74a86dbfa44ba62d433dffccb1
anjanay <anjanayraina326@gmail.com>
2023-01-26 11:35:18 +0530

    onclickhandler to be added

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,9 @@
 import { ethers } from 'ethers';
 
+
+function onClickHandler(){
+
+}
 const Navigation = ({ account, setAccount }) => {
 
     return (
@@ -8,8 +12,21 @@ const Navigation = ({ account, setAccount }) => {
                 <h1>Dappazon</h1>
             </div>
             <input type = "text" className = "nav__search"/>
-
-            <button type = "button" className='nav__connect'>{account.slice(0,5) + '...' + account.slice(38,42 )}</button>
+            {
+               account ? 
+                    (<button type = "button" className='nav__connect'>{account.slice(0,5) + '...' + account.slice(38,42 )}</button>)
+                : (
+                    <button type = "button" className='nav__connect' onClick= {onClickHandler}>Connect Metamask</button>
+                )
+            }
+            
+            <ul className='nav__links'>
+                <li><a href = "#Clothing">Clothing</a></li>
+                <li><a href = "#Jewelry">Jewelry</a></li>
+                <li><a href = "#Electronics">Electronics</a></li>
+                <li><a href = "#Gaming">Gaming</a></li>
+                <li><a href = "#Gaming">Toys</a></li>
+            </ul>
         </nav>
     );
 }
